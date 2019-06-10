417af4302bcd2400330f27dc3d68fc598cf8f929
Gloria Budiman <wahyu.g@gmail.com>
2019-06-10 00:12:56 -0700

    Styling

--- src/components/summaries/SkillSummary.js
+++ src/components/summaries/SkillSummary.js
@@ -3,18 +3,22 @@ import './SkillSummary.scss';
 
 function SkillSummary(props) {
   return(
-    <div className='summary xp-summary'>
-      <div className='summary summary-skill summary-combat'>
-        XYZ
+    <div>
+      <div className='summary xp-summary'>
+        <div className='summary summary-skill summary-combat'>
+          XYZ
+        </div>
+        <div className='summary summary-skill summary-civilized'>
+          XYZ
+        </div>
       </div>
-      <div className='summary summary-skill summary-civilized'>
-        XYZ
-      </div>
-      <div className='summary summary-skill summary-wasteland'>
-        XYZ
-      </div>
-      <div className='summary summary-skill summary-anomaly'>
-        XYZ
+      <div className='summary xp-summary'>
+        <div className='summary summary-skill summary-wasteland'>
+          XYZ
+        </div>
+        <div className='summary summary-skill summary-anomaly'>
+          XYZ
+        </div>
       </div>
     </div>
   )
