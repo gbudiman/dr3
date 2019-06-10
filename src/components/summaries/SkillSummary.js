ffdacb6edf4e87fd7451b89be72e0624f5637a92
Gloria Budiman <wahyu.g@gmail.com>
2019-06-09 22:51:18 -0700

    Barebone summary

---
+++ src/components/summaries/SkillSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function SkillSummary(props) {
+  return(
+    <div className='summary xp-summary'>
+      <div className='summary summary-skill summary-combat'>
+        XYZ
+      </div>
+      <div className='summary summary-skill summary-civilized'>
+        XYZ
+      </div>
+      <div className='summary summary-skill summary-wasteland'>
+        XYZ
+      </div>
+      <div className='summary summary-skill summary-anomaly'>
+        XYZ
+      </div>
+    </div>
+  )
+}
+
+export default SkillSummary;
