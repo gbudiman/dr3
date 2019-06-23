59cc0c8a0a01e553d9e6cb298c565e16ca5fd8ad
Ryan O'Donnell <rythod@gmail.com>
2019-06-23 13:11:28 -0700

    Delete FeedbackPage Comments

--- src/components/FeedbackPage/FeedbackPage.js
+++ src/components/FeedbackPage/FeedbackPage.js
@@ -45,19 +45,4 @@ const FeedbackPage = () => {
   );
 };
 
-/* <div id='disqus_thread' />
-  <script>
-  var disqus_config = function () {
-    this.page.url = 'http://drpaedia3.herokuapp.com'; // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = 'index'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-function () {
-    // DON'T EDIT BELOW THIS LINE
-    var d = document,
-        s = d.createElement('script');
-    s.src = 'https://drpaedia3.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);}
-    </script>   */
-
 export default FeedbackPage;
