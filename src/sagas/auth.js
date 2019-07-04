65aa4b54c2f93e9d843f332a8e29425abdabbde2
Gloria Budiman <wahyu.g@gmail.com>
2019-07-04 15:45:57 -0700

    Fecth characters implementation

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -1,26 +1,38 @@
 import axios from 'axios';
 import { put, takeEvery, all } from 'redux-saga/effects'
 
-const delay = (ms) => new Promise(res => setTimeout(res, ms))
+const api = (path) => { return 'http://devdrdb.dystopiarisingnetwork.com:5000/api/' + path }
+const config = {
+  headers: {
+    'Authorization': null
+  }
+}
 
 const generateToken = async() => {
   try {
     const res = await axios.get(
-      'http://devdrdb.dystopiarisingnetwork.com:5000/api/generateToken', 
+      api('generateToken'), 
       { auth: { username: 'test', password: 'test1234'} });
-    console.log(res);
+
+    config.headers['Authorization'] = 'Bearer ' + res.data.access_token;
   } catch(error) {
-    console.log('zzz')
+    console.log('Authenticaion failure')
     console.log(error);
   }
-  
+}
+
+const fetchCharacters = async() => {
+  try {
+    const res = await axios.get(api('characters'), config)
+  } catch(error) {
+    console.log('Failed to fetch characters')
+  }
 }
 
 function* auth() {
-  let token = generateToken();
+  yield generateToken();
+  yield fetchCharacters();
 }
-
-
 
 export default function* rootSaga() {
   yield all([auth()])
