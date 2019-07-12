fc2b8bbba34fb51509c1ceb7c845a85aac8c91fc
Gloria Budiman <wahyu.g@gmail.com>
2019-07-12 16:08:21 -0700

    Try again?

--- src/utils/CharacterUtil.js
+++ src/utils/CharacterUtil.js
@@ -8,18 +8,9 @@ const CharacterUtil = props => {
   let statUtil = StatUtil();
   let strainUtil = StrainUtil();
   let skillUtil = SkillUtil();
-  let su = props.su;
 
   return (
-    <CharacterPage
-      su={su}
-      passSkillGridClick={skillUtil.handleSkillGridClick}
-      passSkillVisibilityToggle={skillUtil.handleSkillVisibilityToggle}
-      passStrainChange={strainUtil.handleStrainChange}
-      passStatClick={statUtil.handleStatClick}
-      passStatChange={statUtil.handleStatChange}
-      passStatReductionChange={statUtil.handleStatReductionChange}
-    />
+    <CharacterPage />
   );
 };
 
